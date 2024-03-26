78fb843ea27e092e034854650e9aa0144619fe8d
aashishkumarnayak <kumaraashish57627@gmail.com>
2024-03-26 19:27:01 +0530

    .

--- app.js
+++ app.js
@@ -9,10 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(express.json());
-app.use(cors({
-  origin: 'http://localhost:3000'
-}));
+const cors = require('cors');
+const corsOptions ={
+    origin:'http://localhost:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // MongoDB Connection
 
