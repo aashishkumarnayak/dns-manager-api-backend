b6c66bf7973aa25d7f0ef96cb131256ff7823e9c
aashishkumarnayak <kumaraashish57627@gmail.com>
2024-03-26 19:29:24 +0530

    ..

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-const cors = require('cors');
+
 const corsOptions ={
     origin:'http://localhost:3000', 
     credentials:true,            //access-control-allow-credentials:true
