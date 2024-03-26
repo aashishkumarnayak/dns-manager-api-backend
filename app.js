12515b0c825db1d571e8eaa4483ec6d5d2628c0d
aashishkumarnayak <kumaraashish57627@gmail.com>
2024-03-26 22:11:07 +0530

    .

--- app.js
+++ app.js
@@ -10,8 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 
+
 const corsOptions ={
-    origin:'https://dns-manager-aws-route-53.netlify.app/', 
+  origin: 'https://dns-manager-aws-route-53.netlify.app/',
     credentials:true,            //access-control-allow-credentials:true
     optionSuccessStatus:200
 }
